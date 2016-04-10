9c27f825acea78cbeac510cd36f5639b63099478
emforce <emforce@live.com>
2016-04-10 17:25:54 +0100

    Initial API created for lessons

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,12 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::group(['prefix' => 'api/v1'], function (){
+    Route::resource('lessons', 'LessonsController');    
+});
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
