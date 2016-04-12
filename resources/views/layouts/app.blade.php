7921138ccf103fd9f393b1554c3c54ca2de5577f
emforce <emforce@live.com>
2016-04-12 21:19:47 +0100

    Index page typography poc done, commets design finished

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,11 @@
 <html>
     <head>
         <!-- Compiled and minified JavaScript -->
-        <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('/css/prism.css') }}">
         <link rel="stylesheet" href="{{ asset('/css/materialize.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('/css/admin-style.css', Request::secure()) }}">
+        <link rel="stylesheet" href="{{ asset('/css/front-end.css', Request::secure()) }}">
        
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -14,45 +15,40 @@
         
         <!-- Dropdown Structure -->
         <ul id="dropdown1" class="dropdown-content">
-            <li><a href="#!">one</a></li>
-            <li><a href="#!">two</a></li>
+            <li><a href="#!">Developing a Website with Laravel 5.2</a></li>
+            <li><a href="#!">Introduction to Website Security</a></li>
             <li class="divider"></li>
-            <li><a href="#!">three</a></li>
+            <li><a href="#!">OpenGL Game Programming For Beginners</a></li>
         </ul>
         <div class="navbar-fixed">
             <nav>
                 <div class="nav-wrapper">
-                <a href="#" class="brand-logo">TutorialEdge.net</a>
+                <a href="{{ url('/') }}" class="brand-logo">TutorialEdge.net</a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">JavaScript</a></li>
+                    <li><a href="badges.html">Tutorials</a></li>
+                    <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Courses<i class="material-icons right">arrow_drop_down</i></a></li>
                     <li><a class="github-signin" href="#">Register With Github</a></li>
-                    <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
                 </ul>
                 </div>
             </nav>
         </div>
         
-        <div class="content">
-            
-            
-        </div>
+        @yield('content')
         
         <footer class="page-footer">
           <div class="container">
             <div class="row">
               <div class="col l6 s12">
-                <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <h5>TutorialEdge.net</h5>
+                <p class="text-lighten-4">This site is dedicated to bringing you the highest quality courses designed to teach you the ins and outs of programming with various frameworks and programming languages.</p>
               </div>
               <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
+                <h5>Links</h5>
                 <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                  <li><a class="text-lighten-3" href="#!">Contact Me</a></li>
+                  <li><a class="text-lighten-3" href="#!">Blog</a></li>
+                  <li><a class="text-lighten-3" href="#!">About Us</a></li>
+                  <li><a class="text-lighten-3" href="#!">Advertise Here</a></li>
                 </ul>
               </div>
             </div>
@@ -60,13 +56,14 @@
           <div class="footer-copyright">
             <div class="container">
             © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            <a class="text-lighten-4 right" href="#!">More Links</a>
             </div>
           </div>
         </footer>
         
         <!--Import jQuery before materialize.js-->
-        <!--<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>-->
+        <script src="{{ asset('/js/prism.js') }}"></script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script type="text/javascript" src="{{ asset('/js/materialize.min.js') }}"></script>
     </body>    
 </html>
