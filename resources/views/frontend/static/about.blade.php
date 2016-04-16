f3fc9fee3fba33d1e621ac2a8e20ba67ede3b681
emforce <emforce@live.com>
2016-04-16 19:15:23 +0100

    added a bit to the about page, needs finishing

--- resources/views/frontend/static/about.blade.php
+++ resources/views/frontend/static/about.blade.php
@@ -10,6 +10,12 @@
 
 @section('content')
 <div class="content">
-    <p><strong>Originally started in December of 2014</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio eum aspernatur sed deleniti, nisi ducimus sequi quaerat saepe, tempora praesentium aliquam sunt architecto fugit corporis officiis molestiae recusandae esse adipisci.</p>
+    <p><strong>Originally started in December of 2014</strong> this site began as a side hobby of mine and has quickly grown into one of the most successful side projects I've ever worked upon. Since it's conception it's racked up well over 100,000 page views and it's still growing.</p>
+    
+    <p>It's undergone <b>many changes</b> since it was first created and overall I've probably commited well over 1,000 hours towards the site and it's content. This is something I use to hone my own programming skills and I use it as a means of expanding my programming horizons by encouraging me to move into new areas that I've never touched before.</p>
+    
+    <h2>The Code</h2>
+    
+    <p><strong>For those interested</strong> in what the code
 </div>
 @endsection
