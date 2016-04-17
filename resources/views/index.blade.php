a9b8ea2bdc026a71643de193131dbf94da46e17b
emforce <emforce@live.com>
2016-04-17 18:34:57 +0100

    added break and height to front-page course boxes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,6 +38,8 @@ by teaching you some of the latest and greatest programming frameworks.</h3>
         </div>
         @endforeach
         
+        <div class="break"></div>
+        
         <?php echo $lessons->render(); ?>
         
     </div>
