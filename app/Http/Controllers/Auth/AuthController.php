4ff801893a5c0cee24e2128f928740d431d6b36e
emforce <emforce@live.com>
2016-04-17 19:31:58 +0100

    Added redirect to home page on auth controller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -107,7 +107,7 @@ class AuthController extends Controller
 
         Auth::login($authUser, true);
                 
-        return redirect('home');
+        return redirect('/');
     }
 
     /**
