a669a3c3552c523b29fd52fa23257bb14e2b5f93
emforce <emforce@live.com>
2016-04-21 22:25:57 +0100

    added icon to forum

--- resources/views/frontend/forum/index.blade.php
+++ resources/views/frontend/forum/index.blade.php
@@ -11,13 +11,16 @@ The Forum
 @section('content')
 <div class="content">
     <h2>Forum Home</h2>
+    <p>Got any questions or suggestions for the site? Post them hear and let me know what I can do to improve the site!</p>
 </div>
 <div class="gray-container">
     <div class="content">
         <div class="row">
-        <div class="col s12 m12">
+        <div class="col s12 m12 l12">
             <div class="card-panel white">
-            <div class="icon">E</div>
+            <div class="icon">
+                E
+            </div>
             <h5>Forum Post Title</h5>
             <span>I am a very simple card. I am good at containing small bits of information.
             I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
