18a98038f59b4d34c9e472a39d6200c25aecb337
emforce <emforce@live.com>
2016-05-12 21:44:07 +0100

    updated course-single page

--- resources/views/frontend/course/single.blade.php
+++ resources/views/frontend/course/single.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.app')
 
+@section('title')
+{{ $course->title }}
+@endsection
+
+@section('meta')
+<meta name="description" content="{{ $course->description }}">
+@endsection
+
 @section('content')
+<div class="gray-container">
+    <div class="content">
+        <h1>{{ $course->title }}</h1>
+        <div class="info">
+            {{ $course->author }}
+            | {{ date("d M, Y",strtotime($course->created_at)) }}
+        </div>
+    </div>
+</div>
+
 <div class="content">
-    <h2>Stuff</h2>
+    <div class="post-body">
+        {{ $course->description }}
+    </div>
 </div>
 @endsection
