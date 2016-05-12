14d6d7eb6c0f104b955fdbccc1a9d9b5dbc37725
emforce <emforce@live.com>
2016-05-12 21:47:50 +0100

    added a 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title')
+404 Page Not Found.
+@endsection
+
+@section('meta')
+<meta name="description" content="404 Page Not Found">
+@endsection
+
+@section('content')
+<div class="gray-container">
+    <div class="content">
+        <h1>404: Page Not Found</h1>
+    </div>
+</div>
+@endsection
