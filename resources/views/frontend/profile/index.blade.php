8fd945c228918bd12d18fb5fa50f6563267c6ef4
emforce <emforce@live.com>
2016-05-17 19:47:47 +0100

    added some user information

--- resources/views/frontend/profile/index.blade.php
+++ resources/views/frontend/profile/index.blade.php
@@ -8,7 +8,19 @@
 </div>
 
 <div class="content">
-    <p>This is still under construction...</p>
+    <div class="container">
+        <div class="row">
+            
+            <div class="col s6">
+                <p><b>Date Joined:</b> {{ $user->created_at }}</p>
+            </div>
+            
+            <div class="col s6">
+                <p><b>Email:</b> {{ $user->email }}</p>
+            </div>
+            
+        </div>
+    </div>
 </div>
 
 @endsection
