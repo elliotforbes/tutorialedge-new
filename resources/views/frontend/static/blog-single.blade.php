983e94e62053f3579ec5b4100dfdbe4b2fef1ac9
emforce <emforce@live.com>
2016-05-22 09:24:38 +0100

    blog single moved to author and commet partials

--- resources/views/frontend/static/blog-single.blade.php
+++ resources/views/frontend/static/blog-single.blade.php
@@ -33,30 +33,9 @@
     <div class="comments">
         
        @include('frontend.partials._author')
-        
-        @if (Auth::guest())
-         <div class="comment">
-            <div class="icon">
-                !
-            </div>
-            <h5>
-                <a href="#">Access Denied</a>
-                <span class="time-since">
-                    - You need to login or register in order to add your own comments
-                </span>
-            </h5>
-            <p>Register now and get the latest tutorials and courses delivered straight to your mailbox! 
-            <div class="clear"></div>
-        </div>
-        @else
-         <div class="comment">
-            
-            <div class="clear"></div>
-        </div>
-        
-        @endif
+       
+       @include('frontend.partials._comments')
         
     </div>
-    <div class="clear"></div>
 </div>
 @endsection
