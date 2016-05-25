0b2491c4dcae5d88f541b8fe278b90dcfc82b58c
emforce <emforce@live.com>
2016-05-25 21:42:39 +0100

    quick and nasty commit to keep the streak going

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -3,5 +3,15 @@
 @section('content')
 <div class="container">
     <h3>Settings:</h3>
+    <div class="row">
+        <div class="widget">
+            <h4>Page Views:</h4>
+        </div>
+        
+        <div class="widget">
+            <h4>New Users:</h4>
+            
+        </div>
+    </div>
 </div>
 @endsection
