895dfd2805a16061f35a511c6e1423f60dfc12c5
emforce <emforce@live.com>
2016-06-01 06:57:28 +0100

    updated admin css

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
     <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}"/>
-    <link rel="stylesheet" href="{{ asset('/css/admin-style.css?v=134' , Request::secure() ) }}"/>
+    <link rel="stylesheet" href="{{ asset('/css/admin-style.css?v=14' , Request::secure() ) }}"/>
 </head>
 <body>
     
