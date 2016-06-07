a96c5501a32fc5bb1a522fffbae182ac1128abd6
emforce <emforce@live.com>
2016-06-07 10:37:16 +0100

    added a beter description to the 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,8 +10,10 @@
 
 @section('content')
 <div class="gray-container">
-    <div class="content">
+    <div class="content center">
         <h1>404: Page Not Found</h1>
+        
+        <p>We're sorry, we couldn't find the page you were looking for.</p>
     </div>
 </div>
 @endsection
