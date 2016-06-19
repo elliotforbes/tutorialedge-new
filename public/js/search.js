d36ee8c5f2bc5638441eb824364aec73b8563654
emforce <emforce@live.com>
2016-06-19 16:34:50 +0100

    removed console

--- public/js/search.js
+++ public/js/search.js
@@ -6,12 +6,7 @@ angular.module('TutorialApp', [])
         
         $http.get('/api/v1/lessons')
             .success(function (response){
-                console.log("Successfully Retrieve lessons");
                 $scope.results = response;
-                console.log($scope.results);
-            })
-            .error(function (response){
-                 console.log("Failed to retrieve lessons");
             });
     };
     
