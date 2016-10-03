880278cd8ccb6bf16d34c4c09b34b0f4d3887649
emforce <emforce@live.com>
2016-10-03 21:36:30 +0100

    added author to the courses page

--- resources/views/frontend/course/single.blade.php
+++ resources/views/frontend/course/single.blade.php
@@ -11,8 +11,46 @@
 @section('content')
 <div class="content">
     <div class="row">
+        <div class="col s12 m12 l2">
+            <div class="left-sidebar">
+                <div class="left-sidebar-content">
+                    <img src="https://avatars0.githubusercontent.com/u/3332224?v=3&s=466" alt="">
+                    <h2>{!! $lesson->author !!}</h2>
+                    <h4>Software Engineer</h4>
+
+                    <p>23 Year old Software Engineer who loves all manner of front end programing.</p>
+                </div>
+                <div class="social-links">
+                    <h4>Follow Me</h4>
+                    <div class="row">
+                        <div class="col s12 m6 l3">
+                            <div class="twitter">
+                                <a target="_blank" href="https://twitter.com/Elliot_F"><i class="ion ion-social-twitter"></i></a>
+                            </div>
+                        </div>
+                        <div class="col s12 m6 l3">
+                            <div class="facebook">
+                                <a target="_blank" href="https://www.facebook.com/tutorialedge"><i class="ion ion-social-facebook"></i></a>
+                            </div>
+                        </div>
+                        <div class="col s12 m6 l3">
+                            <div class="github">
+                                <a target="_blank" href="https://github.com/emforce"><i class="ion ion-social-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="col s12 m6 l3">
+                            <div class="youtube">
+                                <a target="_blank" href="https://www.youtube.com/channel/UCwFl9Y49sWChrddQTD9QhRA"><i class="ion ion-social-youtube"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+
         
-        <div class="col s12 m8 l8">
+        <div class="col s12 m8 l7">
             <div class="post-body">  
                 <h1>{{ $course->title }}</h1>
                 {!! Markdown::parse($course->body) !!}
@@ -21,7 +59,7 @@
             </div>
         </div><!-- .col -->
         
-        <div class="sidebar col s12 m4 l4">
+        <div class="sidebar col s12 m4 l3">
             <div class="course-info">
                 <h2>Course Information:</h2>
                 <p><b>Author:</b> {{ $course->author }}</p>
